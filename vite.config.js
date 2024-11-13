774f358a2366c90fd6b1c030272b118e036c8e8d
EloySad <johaneloysanchezhernandez@gmail.com>
2024-11-13 11:13:24 -0500

    feat: view login created with components

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
+                'resources/assets/css/app.css',
+                'resources/assets/css/style.bundle.css',
                 'resources/js/app.js',
             ],
             refresh: true,
