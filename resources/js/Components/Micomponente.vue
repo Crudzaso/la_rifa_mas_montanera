2b4c9687b327ff505f374c4e253ab3afacaca710
Darkot1 <sebastian.serna.1998@gmail.com>
2024-11-28 01:21:58 -0500

    chore: reset project, remove old code and add new project structure

---
+++ resources/js/Components/Micomponente.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>Hola desde MiComponente</h2>
+        <p>Este es un componente de prueba.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MiComponente',
+};
+</script>
+
+<style scoped>
+h2 {
+    color: #42b983;
+}
+</style>
