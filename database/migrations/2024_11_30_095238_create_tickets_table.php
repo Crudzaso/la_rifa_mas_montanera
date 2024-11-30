4313b621e9bde4c5585315d7d602ae85d718937d
Darkot1 <sebastian.serna.1998@gmail.com>
2024-11-30 13:01:20 -0500

    feat: Add Tickets model, controller, and migration for ticket management

---
+++ database/migrations/2024_11_30_095238_create_tickets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('raffle_id')->constrained()->onDelete('cascade');
+            $table->char('ticket_number', 2)->unique();
+            $table->boolean('is_winner')->default(false);
+
+            $table->unique(['user_id', 'raffle_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
