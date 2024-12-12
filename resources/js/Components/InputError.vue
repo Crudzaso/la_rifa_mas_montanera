b36d62bc2e207ab7dd8823e4f7adf4fe9ecac76f
Sebastian Serna Arango <161923093+Darkot1@users.noreply.github.com>
2024-12-12 01:41:55 -0500

    Revert "Develop"

--- resources/js/Components/InputError.vue
+++ resources/js/Components/InputError.vue
@@ -5,25 +5,9 @@ defineProps({
 </script>
 
 <template>
-    <div v-show="message" class="flex items-center space-x-2 animate-shake">
-        <svg class="w-4 h-4 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-        </svg>
-        <p class="text-sm font-medium text-red-500">
+    <div v-show="message">
+        <p class="text-sm text-red-600">
             {{ message }}
         </p>
     </div>
 </template>
-
-<style scoped>
-.animate-shake {
-    animation: shake 0.5s cubic-bezier(.36,.07,.19,.97) both;
-}
-
-@keyframes shake {
-    10%, 90% { transform: translate3d(-1px, 0, 0); }
-    20%, 80% { transform: translate3d(2px, 0, 0); }
-    30%, 50%, 70% { transform: translate3d(-2px, 0, 0); }
-    40%, 60% { transform: translate3d(2px, 0, 0); }
-}
-</style>
